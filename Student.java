571ad949c93bf70d4d2a198fca2563b4c0af1da3
kevinrhu <kevinrouhu@gmail.com>
2018-07-07 14:46:23 -0400

    update

--- Student.java
+++ Student.java
@@ -28,5 +28,22 @@ class Student {
         System.out.println("program" + this.program);
         System.out.println("nationality" + this.nationaity);
     }
+
+    void deserialize(String student) {
+        String[] sStudents = sStudent.split("&");
+        this.id = Integer.parseInt(sStudents[0]);
+        this.name = sStudents[1];
+        this.age = Integer.parseInt(sStudents[2]);
+        this.gender = sStudents[3].charAt(0);
+        this.school = sStudents[4];
+        this.program = sStudents[5];
+        this.nationaity = sStudents[6];
+    }
     
+    String serialize(){
+        String sRet =
+                this.id +"&" + this.name+"&"+this.age+"&"+this.gender+"&"+this.school+"&"+this.program+"&"+this.nationaity;
+        return sRet;
+    }
+
 }
--- StudentsInfo.java
+++ StudentsInfo.java
@@ -21,19 +21,22 @@ public class StudentsInfo {
         Scanner scanner;
         try {
             File file = new File("C:\\Users\\Pineapplelord\\Documents\\students.txt");
-            scanner = new Scanner(new BufferedReader(new FileReader(file)));    
+            scanner = new Scanner(new BufferedReader(new FileReader(file)));
         } catch (Exception e) {
             return null;
         }
         for (int i = 0; i < 5; i++) {
             Student student = new Student();
-            student.id = Integer.parseInt(scanner.nextLine());
-            student.name = scanner.nextLine();
-            student.age = Integer.parseInt(scanner.nextLine());
-            student.gender = scanner.nextLine().charAt(0);
-            student.school = scanner.nextLine();
-            student.program = scanner.nextLine();
-            student.nationaity = scanner.nextLine();
+
+            Student sStudent = new Student();
+            student.deserialize(sc.nextLine());
+//            student.id = Integer.parseInt(scanner.nextLine());
+//            student.name = scanner.nextLine();
+//            student.age = Integer.parseInt(scanner.nextLine());
+//            student.gender = scanner.nextLine().charAt(0);
+//            student.school = scanner.nextLine();
+//            student.program = scanner.nextLine();
+//            student.nationaity = scanner.nextLine();
             sts[i] = student;
         }
         return sts;
@@ -53,16 +56,34 @@ public class StudentsInfo {
             System.out.println("File is not Found");
             return;
         }
-        int id = Integer.parseInt(sc.nextLine());
-        while (id > 0) {
-            Student student = findStudentWithId(id);
-            if (student == null) {
-                System.out.println("the id input is not correct. Please input Integer from 1-5");
+        printStudentNo();
+        String cmd = sc.nextLine();
+        while (!cmd.equals('q')) {
+            if (cmd.equals('+')) {
+                addNewStudent();
+            } else if (cmd.equals('-')) {
+                removeStudent();
             } else {
-                student.printInfo();
+                int id = Integer.parseInt(cmd);
+                Student student = findStudentWithId(id);
+                if (student == null) {
+                    System.out.println("The id is not correct. Please input integer from 1-5: ");
+                } else {
+                    student.printInfo();
+                }
             }
-            id = Integer.parseInt(sc.nextLine());
         }
+
+//        int id = Integer.parseInt(sc.nextLine());
+//        while (id > 0) {
+//            Student student = findStudentWithId(id);
+//            if (student == null) {
+//                System.out.println("the id input is not correct. Please input Integer from 1-5");
+//            } else {
+//                student.printInfo();
+//            }
+//            id = Integer.parseInt(sc.nextLine());
+//        }
     }
 
     private static Student findStudentWithId(int id) {
@@ -75,4 +96,50 @@ public class StudentsInfo {
         return null;
     }
 
-}
+    private static void addNewStudent() {
+        Student student = new Student();
+        System.out.println("Add a student info");
+
+        System.out.print(" ID: ");
+        String s = sc.nextLine();
+        student.id = Integer.parseInt(s);
+
+        System.out.print(" Name: ");
+        s = sc.nextLine();
+        student.name = s;
+
+        System.out.print(" Age: ");
+        s = sc.nextLine();
+        student.age = Integer.parseInt(s);
+
+        System.out.print(" Gender: ");
+        s = sc.nextLine();
+        student.gender = s.charAt(0);
+
+        System.out.print(" School: ");
+        s = sc.nextLine();
+        student.school = s;
+
+        System.out.print(" Program: ");
+        s = sc.nextLine();
+        student.program = s;
+
+        System.out.print(" Nationality: ");
+        s = sc.nextLine();
+        student.nationaity = s;
+    }
+
+    private static void removeStudent() {
+        System.out.print("Please input ID: ");
+        int id = sc.nextInt();
+        Student student = findStudentWithId(id);
+        students.remove(student);
+        printStudentNo();
+        
+    }
+
+    private static void printStudentNo() {
+        System.out.println("Now we have " + students.size() + " students");
+    }
+
+}                                                                                                                     
